ad29589de15ce4fe01e510fe40e96d4e88378a9a
siddneyalmeidaa-ai <siddney.almeidaa@gmail.com>
2026-03-24 20:29:07 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,35 @@
-const CACHE_NAME = 'supremacia-v27-cache';
+const CACHE_NAME = 'fenix-prime-v362-cache';
 const assets = [
-  '/FENIX-PRIME-METIS-BOND/',
-  '/FENIX-PRIME-METIS-BOND/index.html',
-  '/FENIX-PRIME-METIS-BOND/manifest.json'
+  './',
+  './index.html',
+  './manifest.json',
+  './logo.png',
+  './tridente.svg'
 ];
 
+// INSTALAÇÃO: SALVA O ESSENCIAL
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(assets)));
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(assets))
+  );
 });
 
+// ATIVAÇÃO: LIMPA O CACHE VELHO (V27 E OUTROS)
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// ESTRATÉGIA: BUSCA NA REDE, SE FALHAR, USA O CACHE
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
-})
-  ;
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+}
+                     );
